a2ade17980cf9ecccd28ef2a2cc81d3055b80667
DEDZTBH <peiqial@outlook.com>
2020-07-03 13:50:25 +0800

    Updates

---
+++ precache-manifest.908609e597ad7fe4efda44e3ab9e5491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8271df0ac746e2e0805fe17983c97927",
+    "url": "/flux/index.html"
+  },
+  {
+    "revision": "ef97e53f85e7927ceb7b",
+    "url": "/flux/static/css/main.4cca5b20.chunk.css"
+  },
+  {
+    "revision": "c66a8aa44c0cfe39368c",
+    "url": "/flux/static/js/2.52961aa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flux/static/js/2.52961aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef97e53f85e7927ceb7b",
+    "url": "/flux/static/js/main.96ccb688.chunk.js"
+  },
+  {
+    "revision": "11b68482b6866750ab72",
+    "url": "/flux/static/js/runtime-main.1b6486f0.js"
+  }
+]);
