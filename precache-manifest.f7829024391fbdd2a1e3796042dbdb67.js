0ba46c30525d8dbb2be7a7df50609991b28c9686
DEDZTBH <peiqial@outlook.com>
2020-07-22 14:20:50 +0800

    Updates

---
+++ precache-manifest.f7829024391fbdd2a1e3796042dbdb67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2023b7e5137de973a40c638460aa1122",
+    "url": "/a-tour-of-color-temperature/index.html"
+  },
+  {
+    "revision": "f643e939fb772a50c3c6",
+    "url": "/a-tour-of-color-temperature/static/css/main.98d37b9d.chunk.css"
+  },
+  {
+    "revision": "40fc02903cc98b9dcbce",
+    "url": "/a-tour-of-color-temperature/static/js/2.10482d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/a-tour-of-color-temperature/static/js/2.10482d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f643e939fb772a50c3c6",
+    "url": "/a-tour-of-color-temperature/static/js/main.7087211c.chunk.js"
+  },
+  {
+    "revision": "7487a91429f87f502960",
+    "url": "/a-tour-of-color-temperature/static/js/runtime-main.cd77d544.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/a-tour-of-color-temperature/precache-manifest.f733ec18d06a6c8c2e938aa31a1216aa.js"
+  "/a-tour-of-color-temperature/precache-manifest.f7829024391fbdd2a1e3796042dbdb67.js"
 );
 
 self.addEventListener('message', (event) => {
